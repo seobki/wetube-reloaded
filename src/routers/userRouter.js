bfa5704ee723a51ada4e73f8f4d8340cc6db1c66
seobki <rltjq0502@naver.com>
2024-04-25 17:57:47 +0900

    github login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userControllers";
+import {
+  edit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userControllers";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
